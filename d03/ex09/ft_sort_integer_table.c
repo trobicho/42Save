fc714915b403faf2d27f812d65f6b1e5e9c969be
Tom ROBICHON <trobicho@e1r6p9.42.fr>
2018-07-06 16:12:51 +0200

    yop

--- d03/ex09/ft_sort_integer_table.c
+++ d03/ex09/ft_sort_integer_table.c
@@ -6,7 +6,7 @@
 /*   By: trobicho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/04 18:07:53 by trobicho          #+#    #+#             */
-/*   Updated: 2018/07/04 18:12:28 by trobicho         ###   ########.fr       */
+/*   Updated: 2018/07/06 14:49:23 by trobicho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@ void	ft_sort_integer_table(int *tab, int size)
 {
 	int i;
 	int j;
+	int tmp;
 
-	for()
+	i=1;
+	while(i<size)
+	{
+		j=i-1;
+		while(j>=0 && tab[j+1]<tab[j])
+		{
+			tmp=tab[j+1];
+			tab[j+1]=tab[j];
+			tab[j]=tmp;
+			j--;
+		}
+		i++;
+	}
 }
--- d04/ex08/ft_eight_queens_puzzle.c
+++ d04/ex08/ft_eight_queens_puzzle.c
@@ -6,11 +6,11 @@
 /*   By: trobicho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/05 20:04:45 by trobicho          #+#    #+#             */
-/*   Updated: 2018/07/05 22:20:36 by trobicho         ###   ########.fr       */
+/*   Updated: 2018/07/06 00:57:04 by trobicho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_eight_queens_puzzle(void)
+int		ft_eight_queens_puzzle(void)
 {
 	int chess_tab[8];
 	int r;
@@ -25,11 +25,13 @@ int ft_eight_queens_puzzle(void)
 	return r;
 }
 
-int ft_check_chess_tab(int chess_tab[])
+int		ft_check_chess_tab(int chess_tab[])
 {
+	int	i;
+
 }
 
-int ft_check_queen_pos(int q1, int q2)
+int		ft_check_queen_pos(int q1, int q2)
 {
 	int r_diff;
 	int c_diff;
@@ -38,18 +40,18 @@ int ft_check_queen_pos(int q1, int q2)
 		return 0;
 	if (q1 % 8 == q2 % 8)
 		return 0;
-	r_diff=q1 / 8 - q2 / 8;
-	c_diff=q1 % 8 - q2 % 8;
+	r_diff = q1 / 8 - q2 / 8;
+	c_diff = q1 % 8 - q2 % 8;
 	if (r_diff < 0)
-		r_diff=-r_diff;
+		r_diff = -r_diff;
 	if (c_diff < 0)
-		c_diff=-c_diff;
+		c_diff = -c_diff;
 	if (r_diff == c_diff)
 		return 0;
 	return 1;
 }
 
-void init(int chess_tab[])
+void	init(int chess_tab[])
 {
 	int i;
 
