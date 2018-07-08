e80b1d763e46b328fb8b3991eadbbecd14c9459e
Tom ROBICHON <trobicho@e1r2p8.42.fr>
2018-07-08 13:12:08 +0200

    yoouhouloyou

---
+++ d05/main/main12.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int	ft_str_is_numeric(char *str);
+
+int	main(int argc, char **argv)
+{
+	printf("%d\n", ft_str_is_numeric(argv[1]));
+}
---
+++ d05/main/main14.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int	ft_str_is_uppercase(char *str);
+
+int	main(int argc, char **argv)
+{
+	printf("%d\n", ft_str_is_uppercase(argv[1]));
+}
